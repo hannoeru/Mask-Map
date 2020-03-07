4d2cdc1241486fdea5840f661c7def343b98cc9f
hanlee <me@hanlee.co>
2020-03-07 23:13:52 +0900

    Change selected style

--- src/components/MapLayer.vue
+++ src/components/MapLayer.vue
@@ -18,7 +18,10 @@
       </vl-layer-vector>
       <vl-geoloc @update:position="geolocPosition = $event">
         <template slot-scope="geoloc">
-          <vl-feature v-if="geoloc.position" id="position-feature">
+          <vl-feature
+            v-if="geoloc.position"
+            id="position-feature"
+          >
             <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
             <vl-style-box>
               <vl-style-icon
@@ -34,70 +37,77 @@
         :features.sync="selectedFeatures"
         ref="interaction"
       >
+        <vl-style-box>
+          <vl-style-icon
+            src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png"
+            :scale="0.4"
+            :anchor="[0.5, 1]"
+          ></vl-style-icon>
+        </vl-style-box>
       </vl-interaction-select>
     </vl-map>
   </div>
 </template>
 
 <script>
-export default {
-  props: ['data'],
-  data() {
-    return {
-      zoom: 11,
-      center: [121.597366, 25.105497],
-      rotation: 0,
-      geolocPosition: undefined,
-      selectedFeatures: [],
-      list: this.data,
-      api: process.env.VUE_APP_MASK_API
-    }
-  },
-  components: {},
-  created() {
-    // this.getUesrLocation();
-  },
-  methods: {
-    getUesrLocation() {
-      navigator.geolocation.getCurrentPosition(location => {
-        this.center = [location.coords.latitude, location.coords.longitude]
-        this.userLocation = [
-          location.coords.latitude,
-          location.coords.longitude
-        ]
-      })
+  export default {
+    props: ["data"],
+    data() {
+      return {
+        zoom: 11,
+        center: [121.597366, 25.105497],
+        rotation: 0,
+        geolocPosition: undefined,
+        selectedFeatures: [],
+        list: this.data,
+        api: process.env.VUE_APP_MASK_API
+      };
     },
-    getIcon(maskNum) {
-      if (maskNum < 100) {
-        return this.greyIcon
-      } else {
-        return this.greenIcon
+    components: {},
+    created() {
+      // this.getUesrLocation();
+    },
+    methods: {
+      getUesrLocation() {
+        navigator.geolocation.getCurrentPosition(location => {
+          this.center = [location.coords.latitude, location.coords.longitude];
+          this.userLocation = [
+            location.coords.latitude,
+            location.coords.longitude
+          ];
+        });
+      },
+      getIcon(maskNum) {
+        if (maskNum < 100) {
+          return this.greyIcon;
+        } else {
+          return this.greenIcon;
+        }
+      },
+      getPoints() {
+        return this.data.map(item => Object.freeze(item.geometry.coordinates));
+      },
+      showInfo(item) {
+        console.log(item);
       }
     },
-    getPoints() {
-      return this.data.map(item => Object.freeze(item.geometry.coordinates))
+    computed: {
+      dataFilter() {
+        return this.data.filter(item => item.properties.county == this.city);
+      }
     },
-    showInfo(item) {
-      console.log(item)
+    watch: {
+      selectedFeatures: function(val) {
+        this.$emit("update-selected", val[0]);
+      }
     }
-  },
-  computed: {
-    dataFilter() {
-      return this.data.filter(item => item.properties.county == this.city)
-    }
-  },
-  watch: {
-    selectedFeatures: function(val) {
-      this.$emit('update-selected', val[0])
-    }
-  }
-}
+  };
 </script>
 <style lang="scss">
-@import '~leaflet/dist/leaflet.css';
-#map {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
+  @import "~leaflet/dist/leaflet.css";
+  #map {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
 </style>
