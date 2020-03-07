b3f630cab081ae84c9db95f89d70f94e60972627
hannoeru <me@hanlee.co>
2020-03-07 16:54:27 +0900

    commit

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,8 +1,19 @@
 <template>
   <v-card height="95vh" class="v-card ma-6">
     <v-card-title class="blue white--text">
-      <span class="headline">尋找藥局</span>
+      <span class="headline mb-4">尋找藥局</span>
       <v-spacer></v-spacer>
+      <v-select
+        :items="citys.counties"
+        label="Outlined style"
+        v-model="selectedCity"
+        outlined
+      ></v-select>
+      <v-select
+        :items="getDistricts()"
+        label="Outlined style"
+        outlined
+      ></v-select>
     </v-card-title>
     <v-card-text>
       <!-- <template v-slot:activator="{ on }">
@@ -11,7 +22,7 @@
           </v-btn>
         </template> -->
       <v-list class="v-list">
-        <v-list-item v-for="(item, i) in data" :key="i">
+        <v-list-item v-for="(item, i) in getData" :key="i">
           <v-list-item-title>{{ item.properties.name }}</v-list-item-title>
         </v-list-item>
       </v-list>
@@ -19,10 +30,34 @@
   </v-card>
 </template>
 <script>
+import citys from '../data-zh'
 export default {
   props: ['data'],
   data() {
-    return {}
+    return {
+      citys: citys,
+      selectedCity: ''
+    }
+  },
+  methods: {
+    getDistricts() {
+      if (this.selectedCity == '') {
+        return []
+      }
+      const data = this.citys.districts[
+        citys.counties.indexOf(this.selectedCity)
+      ]
+      console.log(data)
+      return data[0]
+    }
+  },
+  computed: {
+    getData() {
+      console.log(
+        this.data.filter(item => item.properties.county == this.selectedCity)
+      )
+      return this.data.find(item => item.properties.county == this.selectedCity)
+    }
   }
 }
 </script>
