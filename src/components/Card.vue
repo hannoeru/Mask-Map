52c1225466adf73dd68e25d9ef81bfc5aad68b23
hanlee <me@hanlee.co>
2020-03-10 03:45:03 +0900

    fix component didn't show name

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="card">
     <div class="mask-num">
-      <div
-        class="adult"
-        :class="maskClass(data.properties.mask_adult)"
-      >
+      <div class="adult" :class="maskClass(data.properties.mask_adult)">
         <div class="text">成人口罩數量</div>
         <div class="num">
           {{ data.properties.mask_adult }}<span class="unit">片</span>
         </div>
       </div>
-      <div
-        class="child"
-        :class="maskClass(data.properties.mask_child)"
-      >
+      <div class="child" :class="maskClass(data.properties.mask_child)">
         <div class="text">兒童口罩數量</div>
         <div class="num">
           {{ data.properties.mask_child }}<span class="unit">片</span>
@@ -21,8 +15,8 @@
       </div>
     </div>
     <div class="title">
-      <div class="name">藥局</div>
-      <div class="km">1.2km</div>
+      <div class="name">{{ data.properties.name }}</div>
+      <!-- <div class="km">1.2km</div> -->
       <div class="is-open">
         {{ isOpen(data.properties.available) }}
       </div>
@@ -32,18 +26,16 @@
         <div class="title">地址</div>
         <div class="data">{{ data.properties.address }}</div>
         <div class="check">
-          <a
-            :href="mapUrl(data.properties)"
-            target="_blank"
-          >於地圖查看</a>
+          <a :href="mapUrl(data.properties)" target="_blank">Google Map</a>
         </div>
       </div>
       <div class="line">
         <div class="title">電話</div>
         <div class="data">{{ data.properties.phone }}</div>
         <div class="check">
-          <a :href="('tel:' + data.properties.phone.replace(/\s*/g,''))"></a>
-          撥打電話</div>
+          <a :href="'tel:' + data.properties.phone.replace(/\s*/g, '')"></a>
+          撥打電話
+        </div>
       </div>
       <div class="line">
         <div class="title">備註</div>
@@ -53,175 +45,175 @@
   </div>
 </template>
 <script>
-  export default {
-    props: ["data"],
-    data() {
-      return {};
-    },
-    methods: {
-      maskClass(val) {
-        let c = "";
-        if (val == 0) {
-          c = "none";
-        } else if (val < 1000) {
-          c = "bad";
-        } else {
-          c = "good";
-        }
-        return c;
-      },
-      isOpen(val) {
-        if (val == "") return "未知";
-        const r = val.split("、");
-        for (let i = 0; i < r.length; i++) {
-          r[i] = r[i].substr(5) == "看診" ? true : false;
-        }
-        const timeArray = [r.splice(0, 7), r.splice(0, 7), r.splice(0, 7)];
-        const time = new Date();
-        const hour = time.getHours();
-        const min = time.getMinutes();
-        const date = time.getDate();
-        let zone = 0;
-        if (8 < hour < 11 && min > 30) {
-          zone = 0;
-        } else if (12 < hour < 22 && min < 30) {
-          zone = 1;
-        } else {
-          zone = 2;
-        }
-        return timeArray[zone][date] ? "營業中" : "休息中";
-      },
-      mapUrl(val) {
-        return `https://www.google.com/maps/search/?api=1&query=${val.name} ${val.address}`;
+export default {
+  props: ['data'],
+  data() {
+    return {}
+  },
+  methods: {
+    maskClass(val) {
+      let c = ''
+      if (val == 0) {
+        c = 'none'
+      } else if (val < 1000) {
+        c = 'bad'
+      } else {
+        c = 'good'
       }
+      return c
+    },
+    isOpen(val) {
+      if (val == '') return '未知'
+      const r = val.split('、')
+      for (let i = 0; i < r.length; i++) {
+        r[i] = r[i].substr(5) == '看診' ? true : false
+      }
+      const timeArray = [r.splice(0, 7), r.splice(0, 7), r.splice(0, 7)]
+      const time = new Date()
+      const hour = time.getHours()
+      const min = time.getMinutes()
+      const date = time.getDate()
+      let zone = 0
+      if (8 < hour < 11 && min > 30) {
+        zone = 0
+      } else if (12 < hour < 22 && min < 30) {
+        zone = 1
+      } else {
+        zone = 2
+      }
+      return timeArray[zone][date] ? '營業中' : '休息中'
+    },
+    mapUrl(val) {
+      return `https://www.google.com/maps/search/?api=1&query=${val.name} ${val.address}`
     }
-  };
+  }
+}
 </script>
 <style lang="scss">
-  .center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .card {
-    width: 343px;
-    padding-bottom: 20px;
-    background: #ffffff 0% 0% no-repeat padding-box;
-    box-shadow: 0px 3px 10px #0000001a;
-    border-radius: 10px;
-    > .mask-num {
-      @extend .center;
-      height: 116px;
-      > div {
-        width: 150px;
-        height: 92px;
-        background: #11787a;
-        border-radius: 10px;
-        letter-spacing: 0;
-        text-align: left;
-        color: #ffffff;
-        padding: 7px 16px;
-        > .text {
-          padding-top: 4px;
-          font: 14px/20px Noto Sans CJK TC;
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  width: 343px;
+  padding-bottom: 20px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 10px #0000001a;
+  border-radius: 10px;
+  > .mask-num {
+    @extend .center;
+    height: 116px;
+    > div {
+      width: 150px;
+      height: 92px;
+      background: #11787a;
+      border-radius: 10px;
+      letter-spacing: 0;
+      text-align: left;
+      color: #ffffff;
+      padding: 7px 16px;
+      > .text {
+        padding-top: 4px;
+        font: 14px/20px Noto Sans CJK TC;
+      }
+      > .num {
+        margin-top: 6px;
+        font: Bold 32px/47px Noto Sans CJK TC;
+        > .unit {
+          margin-left: 4px;
+          font: 12px/18px Noto Sans CJK TC;
         }
-        > .num {
-          margin-top: 6px;
-          font: Bold 32px/47px Noto Sans CJK TC;
-          > .unit {
-            margin-left: 4px;
-            font: 12px/18px Noto Sans CJK TC;
-          }
-        }
-      }
-      > .adult {
-        margin-right: 11px;
-      }
-      > .good {
-        background: #11787a;
-      }
-      > .bad {
-        background: #e67e22;
-      }
-      > .none {
-        background: #70777c;
-        opacity: 0.5;
       }
     }
-    > .title {
-      @extend .center;
-      justify-content: flex-start;
-      margin-top: 10px;
-      width: 100%;
+    > .adult {
+      margin-right: 11px;
+    }
+    > .good {
+      background: #11787a;
+    }
+    > .bad {
+      background: #e67e22;
+    }
+    > .none {
+      background: #70777c;
+      opacity: 0.5;
+    }
+  }
+  > .title {
+    @extend .center;
+    justify-content: flex-start;
+    margin-top: 10px;
+    width: 100%;
+    height: 24px;
+    text-align: left;
+    padding: 0px 16px;
+    position: relative;
+    letter-spacing: 0;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
       height: 24px;
-      text-align: left;
-      padding: 0px 16px;
-      position: relative;
+      width: 4px;
+      border-radius: 0px 4px 4px 0px;
+      background: #11787a;
+    }
+    > .name {
+      font: Bold 16px/24px Noto Sans CJK TC;
+      color: #34495e;
+    }
+    > .km {
+      padding-top: 7px;
+      margin-left: 4px;
+      font: Bold 11px/17px Noto Sans CJK TC;
+      color: #566778;
+    }
+    > .is-open {
+      height: 24px;
+      width: 48px;
+      margin-left: 12px;
+      background: rgba(22, 160, 133, 0.1);
+      border-radius: 6px;
+      text-align: center;
+      font: 12px/24px Noto Sans CJK TC;
       letter-spacing: 0;
-      &::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 24px;
-        width: 4px;
-        border-radius: 0px 4px 4px 0px;
-        background: #11787a;
+      color: #16a085;
+    }
+  }
+  > .info {
+    margin-top: 20px;
+    padding: 0px 16px;
+    letter-spacing: 0;
+    > .line {
+      margin-top: 14px;
+      @extend .center;
+      align-items: flex-start;
+      justify-content: flex-start;
+      text-align: left;
+      font: 14px/20px Noto Sans CJK TC;
+      color: #70777c;
+      a:link,
+      a:visited,
+      a:hover,
+      a:active {
+        color: #70777c;
       }
-      > .name {
-        font: Bold 16px/24px Noto Sans CJK TC;
+      > .title {
+        white-space: nowrap;
         color: #34495e;
       }
-      > .km {
-        padding-top: 7px;
-        margin-left: 4px;
-        font: Bold 11px/17px Noto Sans CJK TC;
-        color: #566778;
+      > .data {
+        margin-left: 10px;
       }
-      > .is-open {
-        height: 24px;
-        width: 48px;
-        margin-left: 12px;
-        background: rgba(22, 160, 133, 0.1);
-        border-radius: 6px;
-        text-align: center;
-        font: 12px/24px Noto Sans CJK TC;
-        letter-spacing: 0;
-        color: #16a085;
-      }
-    }
-    > .info {
-      margin-top: 20px;
-      padding: 0px 16px;
-      letter-spacing: 0;
-      > .line {
-        margin-top: 14px;
-        @extend .center;
-        align-items: flex-start;
-        justify-content: flex-start;
-        text-align: left;
-        font: 14px/20px Noto Sans CJK TC;
-        color: #70777c;
-        a:link,
-        a:visited,
-        a:hover,
-        a:active {
-          color: #70777c;
-        }
-        > .title {
-          white-space: nowrap;
-          color: #34495e;
-        }
-        > .data {
-          margin-left: 10px;
-        }
-        > .check {
-          margin-left: auto;
-          white-space: nowrap;
-          font: 12px/18px Noto Sans CJK TC;
-          text-decoration: underline;
-        }
+      > .check {
+        margin-left: auto;
+        white-space: nowrap;
+        font: 12px/18px Noto Sans CJK TC;
+        text-decoration: underline;
       }
     }
   }
+}
 </style>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,53 +1,34 @@
 <template>
-  <div
-    id="menu"
-    :class="{'close' : toggle}"
-  >
-    <div
-      class="cover"
-      v-show='toggle'
-    >
-    </div>
+  <div id="menu" :class="{ close: toggle }">
+    <div class="cover" v-show="toggle"></div>
     <div class="nav">
       <div class="nav_logo">
-        <img
-          src="@/assets/logo.png"
-          alt=""
-        >
+        <img src="@/assets/logo.png" alt="" />
       </div>
       <div class="nav_title">口罩即時查</div>
-      <div
-        class="nav_toggler"
-        @click="(toggle = !toggle)"
-      ><img
-          src="@/assets/ic_toggler@2x.png"
-          alt=""
-        ></div>
+      <div class="nav_toggler" @click="toggle = !toggle">
+        <img src="@/assets/ic_toggler@2x.png" alt="" />
+      </div>
     </div>
     <div class="search">
-      <select
-        name=""
-        v-model="selectedCity"
-      >
+      <select name="" v-model="selectedCity">
         <option
           v-for="(item, key) in cities.counties"
           :key="key"
           :value="item"
-        >{{ item }}</option>
+          >{{ item }}</option
+        >
       </select>
     </div>
     <div class="title_day">
       <div class="text_big">偶數</div>
       <div class="text">購買日</div>
-      <div class="info"><img
-          src="@/assets/ic_help@2x.png"
-          alt=""
-        ></div>
+      <div class="info"><img src="@/assets/ic_help@2x.png" alt="" /></div>
     </div>
     <div class="data_info">
       <div class="info">
-        <span>{{selectedCity}}內的供應商</span>
-        <span>資訊更新時間{{updateTime}}</span>
+        <span>{{ selectedCity }}內的供應商</span>
+        <span>資訊更新時間{{ updateTime }}</span>
       </div>
       <button @click="$parent.getMaskData()">重整列表</button>
     </div>
@@ -60,178 +41,173 @@
           @click.native="updateShow(item)"
         />
       </div>
-
     </div>
   </div>
 </template>
 <script>
-  import cities from "../data-zh";
-  import Card from "./Card";
-  export default {
-    components: { Card },
-    props: ["data"],
-    data() {
-      return {
-        cities: cities,
-        toggle: false,
-        selectedCity: "臺北市",
-        updateTime: "20:22:22"
-      };
-    },
-    methods: {
-      getDistricts() {
-        if (this.selectedCity == "") {
-          return [];
-        }
-        const data = this.cities.districts[
-          cities.counties.indexOf(this.selectedCity)
-        ];
-        console.log(data);
-        return data[0];
-      },
-      updateShow(item) {
-        this.$emit("update-show", item);
-      }
-    },
-    computed: {
-      filterData: function() {
-        console.log(
-          this.data.filter(item => item.properties.county == this.selectedCity)
-        );
-        return this.data.filter(
-          item => item.properties.county == this.selectedCity
-        );
-      }
+import cities from '../data-zh'
+import Card from './Card'
+export default {
+  components: { Card },
+  props: ['data'],
+  data() {
+    return {
+      cities: cities,
+      toggle: false,
+      selectedCity: '臺北市',
+      updateTime: '20:22:22'
     }
-  };
+  },
+  methods: {
+    getDistricts() {
+      if (this.selectedCity == '') {
+        return []
+      }
+      const data = this.cities.districts[
+        cities.counties.indexOf(this.selectedCity)
+      ]
+      return data[0]
+    },
+    updateShow(item) {
+      this.$emit('update-show', item)
+    }
+  },
+  computed: {
+    filterData: function() {
+      return this.data.filter(
+        item => item.properties.county == this.selectedCity
+      )
+    }
+  }
+}
 </script>
 <style lang="scss">
-  select {
-    outline: none;
-    padding: 0px 12px;
-  }
-  .center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .close {
-    transform: translateX(-310px);
-  }
-  #menu {
-    width: 375px;
-    height: 100%;
+select {
+  outline: none;
+  padding: 0px 12px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.close {
+  transform: translateX(-310px);
+}
+#menu {
+  width: 375px;
+  height: 100%;
+  background: #fafafa;
+  position: relative;
+  transition-property: all;
+  transition-duration: 300ms;
+  transition-delay: 0s;
+  transition-timing-function: ease;
+  > .cover {
+    position: absolute;
+    top: 65px;
+    left: 0;
+    height: calc(100% - 65px);
+    width: 100%;
     background: #fafafa;
-    position: relative;
-    transition-property: all;
-    transition-duration: 300ms;
-    transition-delay: 0s;
-    transition-timing-function: ease;
-    > .cover {
-      position: absolute;
-      top: 65px;
-      left: 0;
-      height: calc(100% - 65px);
-      width: 100%;
-      background: #fafafa;
-      z-index: 20;
-    }
-    > .nav {
+    z-index: 20;
+  }
+  > .nav {
+    @extend .center;
+    height: 65px;
+    background: #ffffff;
+    > .nav_logo {
       @extend .center;
-      height: 65px;
-      background: #ffffff;
-      > .nav_logo {
-        @extend .center;
-        margin-left: 16px;
-        margin-right: 10px;
-      }
-      > .nav_title {
-        @extend .center;
-        margin-right: auto;
-      }
-      > .nav_toggler {
-        @extend .center;
-        margin-right: 24px;
-      }
+      margin-left: 16px;
+      margin-right: 10px;
     }
-    > .search {
-      margin-top: 16px;
-      > select {
-        width: 343px;
-        height: 46px;
-        background: #ffffff 0% 0% no-repeat padding-box;
-        border: 1px solid #34495e33;
-        border-radius: 10px;
-      }
-    }
-    > .title_day {
-      display: flex;
-      justify-content: flex-start;
-      align-items: flex-end;
-      margin-top: 12px;
-      padding: 0px 24px;
-      height: 54px;
-      text-align: left;
-      > .text_big {
-        font: Bold 36px/54px Noto Sans CJK TC;
-        letter-spacing: 0;
-        color: #34495e;
-      }
-      > .text {
-        margin-left: 8px;
-        padding-bottom: 7px;
-        font: Regular 16px/24px Noto Sans CJK TC;
-        letter-spacing: 0;
-        color: #34495e;
-      }
-      > .info {
-        margin-left: 8px;
-        margin-bottom: 7px;
-        height: 24px;
-        width: 24px;
-      }
-    }
-    > .data_info {
-      margin-top: 25px;
-      padding: 0px 24px;
+    > .nav_title {
       @extend .center;
-      > .info {
-        @extend .center;
-        flex-direction: column;
-        align-items: flex-start;
-        margin-right: auto;
-        span {
-          text-align: left;
-          font: 12px/18px Noto Sans CJK TC;
-          letter-spacing: 0;
-          color: #566778;
-          opacity: 1;
-        }
-      }
-      button {
-        height: 36px;
-        width: 96px;
-        border: 2px solid #34495e;
-        border-radius: 100px;
-        opacity: 1;
-        font: 14px/20px Noto Sans CJK TC;
-        letter-spacing: 0;
-        color: #34495e;
-        outline: none;
-      }
+      margin-right: auto;
     }
-    > .cards {
+    > .nav_toggler {
+      @extend .center;
+      margin-right: 24px;
+    }
+  }
+  > .search {
+    margin-top: 16px;
+    > select {
+      width: 343px;
+      height: 46px;
+      background: #ffffff 0% 0% no-repeat padding-box;
+      border: 1px solid #34495e33;
+      border-radius: 10px;
+    }
+  }
+  > .title_day {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-end;
+    margin-top: 12px;
+    padding: 0px 24px;
+    height: 54px;
+    text-align: left;
+    > .text_big {
+      font: Bold 36px/54px Noto Sans CJK TC;
+      letter-spacing: 0;
+      color: #34495e;
+    }
+    > .text {
+      margin-left: 8px;
+      padding-bottom: 7px;
+      font: Regular 16px/24px Noto Sans CJK TC;
+      letter-spacing: 0;
+      color: #34495e;
+    }
+    > .info {
+      margin-left: 8px;
+      margin-bottom: 7px;
+      height: 24px;
+      width: 24px;
+    }
+  }
+  > .data_info {
+    margin-top: 25px;
+    padding: 0px 24px;
+    @extend .center;
+    > .info {
       @extend .center;
       flex-direction: column;
-      height: calc(100% - 276px);
-      margin-top: 20px;
-      overflow-y: auto;
-      .card-list {
-        height: 100%;
-      }
-      .card {
-        margin-bottom: 20px;
+      align-items: flex-start;
+      margin-right: auto;
+      span {
+        text-align: left;
+        font: 12px/18px Noto Sans CJK TC;
+        letter-spacing: 0;
+        color: #566778;
+        opacity: 1;
       }
     }
+    button {
+      height: 36px;
+      width: 96px;
+      border: 2px solid #34495e;
+      border-radius: 100px;
+      opacity: 1;
+      font: 14px/20px Noto Sans CJK TC;
+      letter-spacing: 0;
+      color: #34495e;
+      outline: none;
+    }
   }
+  > .cards {
+    @extend .center;
+    flex-direction: column;
+    height: calc(100% - 276px);
+    margin-top: 20px;
+    overflow-y: auto;
+    .card-list {
+      height: 100%;
+    }
+    .card {
+      margin-bottom: 20px;
+    }
+  }
+}
 </style>
