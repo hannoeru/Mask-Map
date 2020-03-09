9183fabc7c48c97ae918d0282455c32a4589230a
hanlee <me@hanlee.co>
2020-03-10 03:21:43 +0900

    Use Crad.vue and Done

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="menu">
+  <div
+    id="menu"
+    :class="{'close' : toggle}"
+  >
+    <div
+      class="cover"
+      v-show='toggle'
+    >
+    </div>
     <div class="nav">
       <div class="nav_logo">
         <img
@@ -10,14 +18,23 @@
       <div class="nav_title">口罩即時查</div>
       <div
         class="nav_toggler"
-        @click="menuToggler"
+        @click="(toggle = !toggle)"
       ><img
           src="@/assets/ic_toggler@2x.png"
           alt=""
         ></div>
     </div>
     <div class="search">
-      <input type="text">
+      <select
+        name=""
+        v-model="selectedCity"
+      >
+        <option
+          v-for="(item, key) in cities.counties"
+          :key="key"
+          :value="item"
+        >{{ item }}</option>
+      </select>
     </div>
     <div class="title_day">
       <div class="text_big">偶數</div>
@@ -28,20 +45,36 @@
         ></div>
     </div>
     <div class="data_info">
-      <span>{{selectedCity}}內的供應商</span>
-      <span>資訊更新時間{{updateTime}}</span>
+      <div class="info">
+        <span>{{selectedCity}}內的供應商</span>
+        <span>資訊更新時間{{updateTime}}</span>
+      </div>
+      <button @click="$parent.getMaskData()">重整列表</button>
     </div>
+    <div class="cards">
+      <div class="card-list">
+        <Card
+          v-for="item in filterData"
+          :key="item.properties.id"
+          :data="item"
+          @click.native="updateShow(item)"
+        />
+      </div>
 
+    </div>
   </div>
 </template>
 <script>
-  import citys from "../data-zh";
+  import cities from "../data-zh";
+  import Card from "./Card";
   export default {
+    components: { Card },
     props: ["data"],
     data() {
       return {
-        citys: citys,
-        selectedCity: "",
+        cities: cities,
+        toggle: false,
+        selectedCity: "臺北市",
         updateTime: "20:22:22"
       };
     },
@@ -50,20 +83,22 @@
         if (this.selectedCity == "") {
           return [];
         }
-        const data = this.citys.districts[
-          citys.counties.indexOf(this.selectedCity)
+        const data = this.cities.districts[
+          cities.counties.indexOf(this.selectedCity)
         ];
         console.log(data);
         return data[0];
       },
-      menuToggler() {}
+      updateShow(item) {
+        this.$emit("update-show", item);
+      }
     },
     computed: {
-      getData() {
+      filterData: function() {
         console.log(
           this.data.filter(item => item.properties.county == this.selectedCity)
         );
-        return this.data.find(
+        return this.data.filter(
           item => item.properties.county == this.selectedCity
         );
       }
@@ -71,15 +106,36 @@
   };
 </script>
 <style lang="scss">
+  select {
+    outline: none;
+    padding: 0px 12px;
+  }
   .center {
     display: flex;
     justify-content: center;
     align-items: center;
   }
+  .close {
+    transform: translateX(-310px);
+  }
   #menu {
     width: 375px;
     height: 100%;
     background: #fafafa;
+    position: relative;
+    transition-property: all;
+    transition-duration: 300ms;
+    transition-delay: 0s;
+    transition-timing-function: ease;
+    > .cover {
+      position: absolute;
+      top: 65px;
+      left: 0;
+      height: calc(100% - 65px);
+      width: 100%;
+      background: #fafafa;
+      z-index: 20;
+    }
     > .nav {
       @extend .center;
       height: 65px;
@@ -100,7 +156,7 @@
     }
     > .search {
       margin-top: 16px;
-      > input {
+      > select {
         width: 343px;
         height: 46px;
         background: #ffffff 0% 0% no-repeat padding-box;
@@ -113,7 +169,7 @@
       justify-content: flex-start;
       align-items: flex-end;
       margin-top: 12px;
-      margin-left: 24px;
+      padding: 0px 24px;
       height: 54px;
       text-align: left;
       > .text_big {
@@ -133,6 +189,48 @@
         margin-bottom: 7px;
         height: 24px;
         width: 24px;
+      }
+    }
+    > .data_info {
+      margin-top: 25px;
+      padding: 0px 24px;
+      @extend .center;
+      > .info {
+        @extend .center;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-right: auto;
+        span {
+          text-align: left;
+          font: 12px/18px Noto Sans CJK TC;
+          letter-spacing: 0;
+          color: #566778;
+          opacity: 1;
+        }
+      }
+      button {
+        height: 36px;
+        width: 96px;
+        border: 2px solid #34495e;
+        border-radius: 100px;
+        opacity: 1;
+        font: 14px/20px Noto Sans CJK TC;
+        letter-spacing: 0;
+        color: #34495e;
+        outline: none;
+      }
+    }
+    > .cards {
+      @extend .center;
+      flex-direction: column;
+      height: calc(100% - 276px);
+      margin-top: 20px;
+      overflow-y: auto;
+      .card-list {
+        height: 100%;
+      }
+      .card {
+        margin-bottom: 20px;
       }
     }
   }
