800cc738f9e582abda723bdd231a14ae11f4624a
hanlee <me@hanlee.co>
2020-03-16 15:29:05 +0900

    No Change

--- src/App.vue
+++ src/App.vue
@@ -20,89 +20,89 @@
 </template>
 
 <script>
-// Import component
-import MapLayer from './components/MapLayer.vue'
-import ShowBox from './components/ShowBox.vue'
-import SideMenu from './components/SideMenu.vue'
-import Loader from './components/Loader.vue'
+  // Import component
+  import MapLayer from "./components/MapLayer.vue";
+  import ShowBox from "./components/ShowBox.vue";
+  import SideMenu from "./components/SideMenu.vue";
+  import Loader from "./components/Loader.vue";
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      isLoading: false,
-      maskData: null,
-      selected: null,
-      show: null,
-      showInfo: false,
-      api: process.env.VUE_APP_MASK_API
-    }
-  },
-  components: {
-    // Loading,
-    MapLayer,
-    ShowBox,
-    SideMenu,
-    Loader
-  },
-  created() {
-    this.getMaskData()
-  },
-  methods: {
-    async getMaskData() {
-      this.isLoading = true
-      const response = await this.axios.get(this.api)
-      console.log('Data Get!')
-      this.maskData = await Object.freeze(response.data.features)
-      this.isLoading = false
+  export default {
+    name: "App",
+    data() {
+      return {
+        isLoading: false,
+        maskData: null,
+        selected: null,
+        show: null,
+        showInfo: false,
+        api: process.env.VUE_APP_MASK_API
+      };
     },
-    updateSelected(item) {
-      this.selected = item
+    components: {
+      // Loading,
+      MapLayer,
+      ShowBox,
+      SideMenu,
+      Loader
     },
-    updateShow(item) {
-      this.show = item
-      this.selected = item
+    created() {
+      this.getMaskData();
+    },
+    methods: {
+      async getMaskData() {
+        this.isLoading = true;
+        const response = await this.axios.get(this.api);
+        console.log("Data Get!");
+        this.maskData = await Object.freeze(response.data.features);
+        this.isLoading = false;
+      },
+      updateSelected(item) {
+        this.selected = item;
+      },
+      updateShow(item) {
+        this.show = item;
+        this.selected = item;
+      }
     }
-  }
-}
+  };
 </script>
 
 <style lang="scss">
-@import '@/assets/main.scss';
-html {
-  overflow: hidden;
-}
-* {
-  box-sizing: border-box;
-}
-html,
-body,
-#app {
-  height: 100%;
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
-}
-p {
-  margin: 0;
-}
-button {
-  outline: none;
-}
-#app {
-  text-align: center;
-  color: #2c3e50;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  background: rgba(250, 250, 250, 10);
-  font: 16px/19px Noto Sans TC;
-}
-#map {
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  z-index: 1;
-}
+  @import "@/assets/main.scss";
+  html {
+    overflow: hidden;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  html,
+  body,
+  #app {
+    height: 100%;
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  p {
+    margin: 0;
+  }
+  button {
+    outline: none;
+  }
+  #app {
+    text-align: center;
+    color: #2c3e50;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background: rgba(250, 250, 250, 10);
+    font: 16px/19px Noto Sans TC;
+  }
+  #map {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    z-index: 1;
+  }
 </style>
